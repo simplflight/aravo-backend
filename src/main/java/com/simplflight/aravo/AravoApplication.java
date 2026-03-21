db1c65bdeecd9e2f6fc3204ac01f1e4744cc2d43
fmacedosantos <felipemacedo986@gmail.com>
2026-03-21 13:01:26 -0300

    Initial commit

---
+++ src/main/java/com/simplflight/aravo/AravoApplication.java
@@ -0,0 +1,13 @@
+package com.simplflight.aravo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AravoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AravoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplflight/aravo/AravoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplflight.aravo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AravoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
