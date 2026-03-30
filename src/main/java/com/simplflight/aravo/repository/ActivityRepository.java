7163ea96782960e6aa0b3271c3a183adaf60a5fd
fmacedosantos <felipemacedo986@gmail.com>
2026-03-29 21:45:10 -0300

    fix: adiciona as annotations 'Repository' para ActivityRepository e ItemRepository

--- src/main/java/com/simplflight/aravo/repository/ActivityRepository.java
+++ src/main/java/com/simplflight/aravo/repository/ActivityRepository.java
@@ -2,10 +2,12 @@ package com.simplflight.aravo.repository;
 
 import com.simplflight.aravo.domain.entity.Activity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface ActivityRepository extends JpaRepository<Activity, UUID> {
 
     List<Activity> findAllByUserId(UUID userId);
--- src/main/java/com/simplflight/aravo/repository/ItemRepository.java
+++ src/main/java/com/simplflight/aravo/repository/ItemRepository.java
@@ -2,8 +2,10 @@ package com.simplflight.aravo.repository;
 
 import com.simplflight.aravo.domain.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ItemRepository extends JpaRepository<Item, UUID> {
 }
